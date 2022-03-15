86e0071c19d242f9493e1d50b887447dedf98780
Daniel <DanielCarrete_Dev@outlook.com>
2022-03-15 12:31:51 -0600

    integral trapecio

--- derivada.py
+++ derivada.py
@@ -1,18 +1,18 @@
 import numpy as np
 
-def f_x(x):
+def f_1x(x):
     val = 2-(np.e**np.cos(x))
     return val
 
-def f1(x,h):
+def f1(x,h,f_x):
     return (f_x(x+h)-f_x(x-h))/(2*h)
 
-def f2(x,h):
+def f2(x,h,f_x):
     return (f_x(x+h)-2*f_x(x)+f_x(x-h))/(h**2)
 
 punto = float(input("En que punto quieres evaluar   "))
 
 print(f"""
-la primera derivada es {f1(punto,0.000001):.5f}
-la segunda derivada es {f2(punto,0.000001):.5f}
+la primera derivada es {f1(punto,0.000001,f_1x):.5f}
+la segunda derivada es {f2(punto,0.000001,f_1x):.5f}
 """)
---
+++ trapecio.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def funcion1(x):
+    return (np.e**(x**2))*np.cos(x)
+
+def funcion2(x):
+    return (x**np.log(x))-2*x
+
+def f2_x(f,x,h):
+    return (f(x+h)-2*f(x)+f(x-h))/(h**2)
+
+def trapecio(f,a,b):
+    h = b-a
+    x = (a + b)/2
+    print(f2_x(f,x,0.000001))
+    return ((h/2)*(f(a)+f(b)))-((h**3)/12)*f2_x(f,x,0.000001)
+  
+def integral(f,a,b,metodo):
+    n = int((b-a)/0.001)+1
+    h = (b-a)/n
+    sum = 0
+    for i in range(n):
+        sum += metodo(f,a,(a+h))
+        a += h
+    return sum
+
+print("definde un intervalo")
+a = float(input("valor de a:  "))
+b = float(input("valor de b:  "))
+
+print(f"El area es aproximadamente {integral(funcion1,0,1,trapecio):.6f}")
